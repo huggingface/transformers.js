214bb788c589c7c08f53568c88f41c0317f3f801
Joshua Lochner <admin@xenova.com>
2024-11-03 06:16:51 +0000

    Add support for OLMo models

--- tests/tiny_random.test.js
+++ tests/tiny_random.test.js
@@ -23,6 +23,7 @@ import {
 
   // Models
   LlamaForCausalLM,
+  OlmoForCausalLM,
   GraniteForCausalLM,
   CohereModel,
   CohereForCausalLM,
@@ -1030,6 +1031,57 @@ describe("Tiny random models", () => {
           MAX_TEST_TIME,
         );
       }
+    });
+  });
+
+  describe("olmo", () => {
+    describe("OlmoForCausalLM", () => {
+      const model_id = "onnx-community/tiny-random-olmo-hf";
+      /** @type {OlmoForCausalLM} */
+      let model;
+      /** @type {GPTNeoXTokenizer} */
+      let tokenizer;
+      beforeAll(async () => {
+        model = await OlmoForCausalLM.from_pretrained(model_id, {
+          // TODO move to config
+          ...DEFAULT_MODEL_OPTIONS,
+        });
+        tokenizer = await GPTNeoXTokenizer.from_pretrained(model_id);
+        tokenizer.padding_side = "left";
+      }, MAX_MODEL_LOAD_TIME);
+
+      it(
+        "batch_size=1",
+        async () => {
+          const inputs = tokenizer("hello");
+          const outputs = await model.generate({
+            ...inputs,
+            max_length: 10,
+          });
+          expect(outputs.tolist()).toEqual([[25521n, 10886n, 44936n, 38777n, 33038n, 18557n, 1810n, 33853n, 9517n, 28892n]]);
+        },
+        MAX_TEST_EXECUTION_TIME,
+      );
+
+      it(
+        "batch_size>1",
+        async () => {
+          const inputs = tokenizer(["hello", "hello world"], { padding: true });
+          const outputs = await model.generate({
+            ...inputs,
+            max_length: 10,
+          });
+          expect(outputs.tolist()).toEqual([
+            [1n, 25521n, 10886n, 44936n, 38777n, 33038n, 18557n, 1810n, 33853n, 9517n],
+            [25521n, 1533n, 37199n, 27362n, 30594n, 39261n, 8824n, 19175n, 8545n, 29335n],
+          ]);
+        },
+        MAX_TEST_EXECUTION_TIME,
+      );
+
+      afterAll(async () => {
+        await model?.dispose();
+      }, MAX_MODEL_DISPOSE_TIME);
     });
   });
 
