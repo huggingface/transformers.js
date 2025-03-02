e41d86115b2f197ed6ed530afe236d3fed21e54b
Joshua Lochner <admin@xenova.com>
2025-03-02 01:25:44 +0000

    Add separated unit tests

--- tests/models/mimi/test_modeling_mimi.js
+++ tests/models/mimi/test_modeling_mimi.js
@@ -1,4 +1,4 @@
-import { EncodecFeatureExtractor, MimiModel } from "../../../src/transformers.js";
+import { EncodecFeatureExtractor, MimiModel, MimiEncoderModel, MimiDecoderModel } from "../../../src/transformers.js";
 
 import { MAX_MODEL_LOAD_TIME, MAX_TEST_EXECUTION_TIME, MAX_MODEL_DISPOSE_TIME, DEFAULT_MODEL_OPTIONS } from "../../init.js";
 
@@ -40,6 +40,49 @@ export default () => {
 
     afterAll(async () => {
       await model?.dispose();
+    }, MAX_MODEL_DISPOSE_TIME);
+  });
+
+  describe("MimiEncoderModel and MimiDecoderModel", () => {
+    const model_id = "hf-internal-testing/tiny-random-MimiModel";
+
+    /** @type {MimiEncoderModel} */
+    let encoder_model;
+    /** @type {MimiDecoderModel} */
+    let decoder_model;
+    /** @type {EncodecFeatureExtractor} */
+    let feature_extractor;
+    let inputs;
+    let encoder_outputs;
+
+    beforeAll(async () => {
+      encoder_model = await MimiEncoderModel.from_pretrained(model_id, DEFAULT_MODEL_OPTIONS);
+      decoder_model = await MimiDecoderModel.from_pretrained(model_id, DEFAULT_MODEL_OPTIONS);
+      feature_extractor = await EncodecFeatureExtractor.from_pretrained(model_id);
+      inputs = await feature_extractor(new Float32Array(12000));
+    }, MAX_MODEL_LOAD_TIME);
+
+    it(
+      "encode",
+      async () => {
+        encoder_outputs = await encoder_model(inputs);
+        expect(encoder_outputs.audio_codes.dims).toEqual([1, encoder_model.config.num_quantizers, 7]);
+      },
+      MAX_TEST_EXECUTION_TIME,
+    );
+
+    it(
+      "decode",
+      async () => {
+        const { audio_values } = await decoder_model(encoder_outputs);
+        expect(audio_values.dims).toEqual([1, 1, 13440]);
+      },
+      MAX_TEST_EXECUTION_TIME,
+    );
+
+    afterAll(async () => {
+      await encoder_model?.dispose();
+      await decoder_model?.dispose();
     }, MAX_MODEL_DISPOSE_TIME);
   });
 };
