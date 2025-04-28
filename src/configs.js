04f39df66b297f67235f77fef83b2b97245f0b2d
Joshua Lochner <26504141+xenova@users.noreply.github.com>
2025-04-28 18:19:36 -0400

    Fix qwen3 config

--- src/configs.js
+++ src/configs.js
@@ -117,7 +117,6 @@ function getNormalizedConfig(config) {
         case 'starcoder2':
         case 'qwen2':
         case 'qwen2_vl':
-        case 'qwen3':
         case 'phi':
         case 'phi3':
         case 'phi3_v':
@@ -126,6 +125,7 @@ function getNormalizedConfig(config) {
             mapping['hidden_size'] = 'hidden_size';
             mapping['num_attention_heads'] = 'num_attention_heads';
             break;
+        case 'qwen3':
         case 'gemma':
         case 'gemma2':
         case 'gemma3_text':
