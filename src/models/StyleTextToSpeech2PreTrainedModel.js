2ac9e84d27ba7f09076369eaf806ebc066303822
Nico Martin <mail@nico.dev>
2025-12-19 12:02:38 +0100

    added model class files

---
+++ src/models/StyleTextToSpeech2PreTrainedModel.js
@@ -0,0 +1,4 @@
+import { PreTrainedModel } from '../models.js';
+
+export class StyleTextToSpeech2PreTrainedModel extends PreTrainedModel {}
+export class StyleTextToSpeech2Model extends StyleTextToSpeech2PreTrainedModel {}
---
+++ src/models/VitsPreTrainedModel.js
@@ -0,0 +1,39 @@
+import { PreTrainedModel } from '../models.js';
+import { VitsModelOutput } from './_base.js';
+
+export class VitsPreTrainedModel extends PreTrainedModel {}
+
+/**
+ * The complete VITS model, for text-to-speech synthesis.
+ *
+ * **Example:** Generate speech from text with `VitsModel`.
+ * ```javascript
+ * import { AutoTokenizer, VitsModel } from '@huggingface/transformers';
+ *
+ * // Load the tokenizer and model
+ * const tokenizer = await AutoTokenizer.from_pretrained('Xenova/mms-tts-eng');
+ * const model = await VitsModel.from_pretrained('Xenova/mms-tts-eng');
+ *
+ * // Run tokenization
+ * const inputs = tokenizer('I love transformers');
+ *
+ * // Generate waveform
+ * const { waveform } = await model(inputs);
+ * // Tensor {
+ * //   dims: [ 1, 35328 ],
+ * //   type: 'float32',
+ * //   data: Float32Array(35328) [ ... ],
+ * //   size: 35328,
+ * // }
+ * ```
+ */
+export class VitsModel extends VitsPreTrainedModel {
+    /**
+     * Calls the model on new inputs.
+     * @param {Object} model_inputs The inputs to the model.
+     * @returns {Promise<VitsModelOutput>} The outputs for the VITS model.
+     */
+    async _call(model_inputs) {
+        return new VitsModelOutput(await super._call(model_inputs));
+    }
+}
