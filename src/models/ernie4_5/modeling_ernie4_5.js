1b01d59a7f77d12d5aaf745016130a082f2a4529
Nico Martin <mail@nico.dev>
2026-01-12 16:01:34 +0100

    Update src/models/ernie4_5/modeling_ernie4_5.js
    Co-authored-by: Joshua Lochner <admin@xenova.com>

--- src/models/ernie4_5/modeling_ernie4_5.js
+++ src/models/ernie4_5/modeling_ernie4_5.js
@@ -1,7 +1,7 @@
 import { PreTrainedModel } from '../modeling_utils.js';
 
-export class Ernie4_5_PretrainedModel extends PreTrainedModel {}
+export class Ernie4_5PretrainedModel extends PreTrainedModel {}
 
-export class Ernie4_5_Model extends Ernie4_5_PretrainedModel {}
+export class Ernie4_5Model extends Ernie4_5PretrainedModel {}
 
-export class Ernie4_5_ForCausalLM extends Ernie4_5_PretrainedModel {}
+export class Ernie4_5ForCausalLM extends Ernie4_5PretrainedModel {}
