00003972f34c521a806ea82d2daf4f4ecea1d267
Joshua Lochner <26504141+xenova@users.noreply.github.com>
2026-03-10 12:17:12 -0400

    Refactor encode functions (use base class)

--- packages/transformers/src/models/lfm2_vl/modeling_lfm2_vl.js
+++ packages/transformers/src/models/lfm2_vl/modeling_lfm2_vl.js
@@ -1,5 +1,4 @@
 import { LlavaForConditionalGeneration } from '../llava/modeling_llava.js';
-import { sessionRun } from '../session.js';
 
 export class Lfm2VlForConditionalGeneration extends LlavaForConditionalGeneration {
     forward_params = [
@@ -11,14 +10,4 @@ export class Lfm2VlForConditionalGeneration extends LlavaForConditionalGeneratio
         'position_ids',
         'past_key_values',
     ];
-
-    async encode_image({ pixel_values, pixel_attention_mask, spatial_shapes }) {
-        return (
-            await sessionRun(this.sessions['vision_encoder'], {
-                pixel_values,
-                pixel_attention_mask,
-                spatial_shapes,
-            })
-        ).image_features;
-    }
 }
